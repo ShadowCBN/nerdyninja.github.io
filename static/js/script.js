bf6fd49a60fb6476a83e925a1a11ccb223df4889
nerdyninja <omkar.ninja@gmail.com>
2018-07-03 21:23:43 +0530

    🌑🌑🌑

--- static/js/script.js
+++
@@ -1,9 +0,0 @@
-function loadAbout() {
-	window.location.href = 'about.html'
-}
-function loadCP() {
-	window.location.href = 'cp.html'
-}
-function loadPhotos() {
-	window.location.href = 'photos.html'
-}
